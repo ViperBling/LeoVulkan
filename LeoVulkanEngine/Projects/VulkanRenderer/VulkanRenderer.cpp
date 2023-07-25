9e25ec644af67826028ebbe216b72d70171fc282
ViperBling <qiusongtao2010@hotmail.com>
2023-07-25 23:32:07 +0800

    [M] Update Renderer

--- LeoVulkanEngine/Projects/VulkanRenderer/VulkanRenderer.cpp
+++ LeoVulkanEngine/Projects/VulkanRenderer/VulkanRenderer.cpp
@@ -1,1 +1,79 @@
 ﻿#include "VulkanRenderer.h"
+
+
+VulkanRenderer::VulkanRenderer(bool msaa, bool enableValidation)
+{
+
+}
+
+VulkanRenderer::~VulkanRenderer()
+{
+
+}
+
+void VulkanRenderer::GetEnabledFeatures()
+{
+    VKRendererBase::GetEnabledFeatures();
+}
+
+void VulkanRenderer::BuildCommandBuffers()
+{
+    VKRendererBase::BuildCommandBuffers();
+}
+
+void VulkanRenderer::Render()
+{
+
+}
+
+void VulkanRenderer::ViewChanged()
+{
+    VKRendererBase::ViewChanged();
+}
+
+void VulkanRenderer::OnUpdateUIOverlay(LeoVK::UIOverlay *overlay)
+{
+    VKRendererBase::OnUpdateUIOverlay(overlay);
+}
+
+void VulkanRenderer::LoadScene(std::string filename)
+{
+    std::cout << "Loading scene from: " << filename << std::endl;
+    mRenderScene.mScene.Destroy(mDevice);
+    auto tStart = std::chrono::high_resolution_clock::now();
+    mRenderScene.mScene.LoadFromFile(filename, mpVulkanDevice, mQueue);
+    auto tFileLoad = std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - tStart).count();
+    std::cout << "Loading took " << tFileLoad << " ms" << std::endl;
+}
+
+void VulkanRenderer::LoadAssets()
+{
+
+}
+
+void VulkanRenderer::SetupDescriptors()
+{
+
+}
+
+void VulkanRenderer::PreparePipelines()
+{
+
+}
+
+void VulkanRenderer::PrepareUniformBuffers()
+{
+
+}
+
+void VulkanRenderer::UpdateUniformBuffers()
+{
+
+}
+
+void VulkanRenderer::PreRender()
+{
+
+}
+
+
--- LeoVulkanEngine/Projects/VulkanRenderer/VulkanRenderer.h
+++ LeoVulkanEngine/Projects/VulkanRenderer/VulkanRenderer.h
@@ -1,2 +1,38 @@
 #pragma once
 
+#include "ProjectPCH.hpp"
+
+#include "VKRendererBase.hpp"
+
+#define ENABLE_VALIDATION true
+#define ENABLE_MSAA true
+
+struct RenderScene
+{
+    LeoVK::GLTFScene mScene;
+    LeoVK::GLTFScene mSkybox;
+};
+
+class VulkanRenderer : public VKRendererBase
+{
+public:
+    VulkanRenderer(bool msaa, bool enableValidation);
+    virtual ~VulkanRenderer();
+    void GetEnabledFeatures() override;
+    void BuildCommandBuffers() override;
+    void Render() override;
+    void ViewChanged() override;
+    void OnUpdateUIOverlay(LeoVK::UIOverlay* overlay) override;
+
+    void LoadScene(std::string filename);
+    void LoadAssets();
+    void SetupDescriptors();
+    void PreparePipelines();
+    void PrepareUniformBuffers();
+    void UpdateUniformBuffers();
+    void PreRender();
+
+public:
+    RenderScene mRenderScene;
+
+};
